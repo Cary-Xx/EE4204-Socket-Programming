a6a39f8d80327a030c5b111f1b115b0cb4262521
Cary__Xx <e0140856@u.nus.edu>
2018-11-15 01:47:22 +0800

    no message

--- Ex2/tcp_ser2.c
+++ Ex2/tcp_ser2.c
@@ -13,7 +13,7 @@ int main(void)
 	int sockfd, con_fd, ret;
 	struct sockaddr_in my_addr;
 	struct sockaddr_in their_addr;
-	int sin_size;
+	uint sin_size;
 	pid_t pid;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);          //create socket
